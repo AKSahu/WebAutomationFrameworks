c76288aa8e58b5e15f6769784f87933174482b38
Aswini Kumar Sahu <aswinikumar.89@gmail.com>
2015-10-28 18:38:21 +0530

    Enabled for safari browser support

---
+++ SeleniumCoreAF/src/coreaf/ui/test/TestDemoSafari.java
@@ -0,0 +1,37 @@
+package coreaf.ui.test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import coreaf.framework.base.DriverManager;
+import coreaf.framework.util.TestEnvironment;
+
+/**
+ * A test class to use parallel execution
+ * 
+ * @author A. K. Sahu
+ *
+ */
+public class TestDemoSafari {
+
+	@Test
+	public void testMethod1() {
+		invokeBrowser("http://www.flipkart.com");
+	}
+
+	@Test
+	public void testMethod2() {
+		invokeBrowser("http://www.ebay.com");
+
+	}
+
+	private void invokeBrowser(String url) {
+		System.out.println("Thread id = " + Thread.currentThread().getId());
+
+		WebDriver driver = DriverManager.getDriver();
+		driver.get(url);
+		String browser = TestEnvironment.getCurrentBrowserName(driver);
+
+		System.out.println("Browser:" + browser);
+	}
+}
