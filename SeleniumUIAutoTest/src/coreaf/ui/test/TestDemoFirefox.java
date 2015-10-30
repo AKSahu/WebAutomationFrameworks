06711a6ad493418f99d694570c138eac960633dd
Aswini Kumar Sahu <aswinikumar.89@gmail.com>
2015-10-30 16:55:25 +0530

    Created a test automation project w.r.t SeleniumCoreAF base framework

---
+++ SeleniumUIAutoTest/src/coreaf/ui/test/TestDemoFirefox.java
@@ -0,0 +1,26 @@
+package coreaf.ui.test;
+
+import org.testng.annotations.Test;
+
+import coreaf.framework.base.BasePage;
+import coreaf.ui.base.TestBase;
+
+/**
+ * A test class to use parallel execution
+ * 
+ * @author A. K. Sahu
+ *
+ */
+public class TestDemoFirefox extends TestBase {
+
+	@Test
+	public void testMethod1() {
+		BasePage.get("http://knowledgebase-wiki.appspot.com/");
+	}
+
+	@Test
+	public void testMethod2() {
+		BasePage.get("http://aksahu.blogspot.in/");
+	}
+
+}
---
+++ SeleniumUIAutoTest/src/coreaf/ui/test/TestDemoSafari.java
@@ -0,0 +1,25 @@
+package coreaf.ui.test;
+
+import org.testng.annotations.Test;
+
+import coreaf.framework.base.BasePage;
+import coreaf.ui.base.TestBase;
+
+/**
+ * A test class to use parallel execution
+ * 
+ * @author A. K. Sahu
+ *
+ */
+public class TestDemoSafari extends TestBase {
+
+	@Test
+	public void testMethod1() {
+		BasePage.get("http://www.microsoft.com/");
+	}
+
+	@Test
+	public void testMethod2() {
+		BasePage.get("http://www.apple.com/");
+	}
+}
